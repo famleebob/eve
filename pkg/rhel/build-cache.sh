25b82017fd0001776772fd59acc0621e22a57a29
Gerald (Bob) Lee <bob@famleehouse.net>
2023-08-18 17:15:03 -0700

    pkg/rhel: first updated/fixes
    `linuxkit` requires everything committed to `git` to process it.
    Thus had to make the initial commit.
    
    Focus to get a single version of `go` across the build and the
    application space.  Created a directory in the builder which
    contains the architectures and go version in files.  Also,
    includes tar file for both the builder architecture, and the
    target architecture.  Tried to use `BUILDARCH`, but it is the
    machine you are building on, not that of the build container
    (may be emulation, oh say amd64).
    
    Updated the `yum` commands, using the new and improved `dnf`.
    `yum` was deprecated in RHEL 8, with symlinks for backward
    compatibility.  RHEL 9 provides that backward compatibility,
    but pressing ahead.  Setting the configuration file for `dnf`
    up front to ignore weak dependencies and some other settings.
    `eve-rhel-deploy.sh` only pull packages from the cache, to it
    fails if any package is missing.  `dnf` will not download with
    an install; it requires a `download` command to pull those
    down.
    
    Updated the file names in `pkg/rhel/mirrors` to reflect the
    RHEL package sections (not certain of the names RH has for
    these sections or groups if you will).  Added sufficient packages
    for `pkg/fscrypt` until the missing `pam-devel` package is
    strictly required.  Used `pkg/fscrypt` as it uses `go` to build,
    and allowed for testing of the set-up and build of the
    EVE package (will pass when we can get `pam-devel`).
    
    Removed `pam-devel` from the `pkg/fscrypt` list of BUILD_PKGS,
    did the build (builds a number of `go` files) that dies
    when `pam-devel`'s missing header files are needed for the
    compilation.  Give good assurance that the build container
    is working correctly, and most work moving forward is enhancement
    or adding packages to the list.
    
    Signed-off-by: Gerald (Bob) Lee <bob@famleehouse.net>

--- pkg/rhel/build-cache.sh
+++ pkg/rhel/build-cache.sh
@@ -8,36 +8,63 @@ bail() {
 
 [ "$#" -gt 2 ] || bail "Usage: $0 <os version> <path to the cache> [packages...]"
 
-REPO_FILE=" $1"
-SUSE_REPO="$(cat /etc/dnf/cache.url)/v$1"
-MY_ARCH="$(uname -m | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)"
-MY_ARCH=${MY_ARCH##*-}
-CACHE="$2/${MY_ARCH}"
-ROOTFS="${CACHE}/../rootfs"
+DNF_CACHE_LOC=/var/cache/dnf
+#** file name must match the package section name
+RHEL_PKG_SECT=$1
+DNF_CACHE="$(find "${DNF_CACHE_LOC}" -type d -print | \
+          grep "${rhel_product}-${releasever_major}-${RHEL_PKG_SECT}" | \
+          grep -v repodata )"
+#** the packages directory does not exist until the first download
+#**  even `dnf makecache` doesn't create the packages directory
+DNF_CACHE="${DNF_CACHE}"/packages
 shift 2
 
-# optionally initialize the cache
-[ ! -d "$CACHE" ] && mkdir -p "$CACHE"
+# ensure package cache exists
+if [ ! -d "${DNF_CACHE}" ]; then
+  rm -rf "${DNF_CACHE}"
+  mkdir -p "${DNF_CACHE}"
+fi
 
 # check for existing packages in the cache: we NEVER overwrite packages
 for p in "$@"; do
-  [ -f "$(echo "$CACHE/${p}"-[0-9]*)" ] || PKGS="$PKGS $p"
+  [ -f "$(echo "${DNF_CACHE}/${p}"-[0-9]*)" ] || PKGS="$PKGS $p"
 done
 
 # fetch the missing packages
 # shellcheck disable=SC2086
 if [ -n "$PKGS" ]; then
-   #  Get single package, and dependencies.  Following should
-   #   just download the set of packages needed
-   yum --assumeyes --nodocs --quiet --noautoremove --downloadonly \
-       install $PKGS
+   rm -f in_output
+   touch in_output
+   #  Get packages and dependencies.  Following should
+   #   download the set of packages needed
+   dnf --assumeyes --nodocs --downloadonly \
+            --setopt=install_weak_deps=False install $PKGS 2>&1 | \
+       tee in_output
 
-   echo -n "${REPO_FILE} Package count "
-   find /var/cache/dnf -name \*.rpm | wc
-   echo "========"
+   #** download packages already installed, so they are in the cache
+   fix_list="$(grep 'already installed' in_output | \
+               sed -e's/Package \(.*\) is already installed.*$/\1/')"
+   if [ -n "${fix_list}" ]; then
+      for xpkg in ${fix_list}
+      do
+         echo "cache=\"${DNF_CACHE}\" xpkg = \"${xpkg}\""
+         # --resolve should pull in dependencies
+         dnf download --resolve --setopt=install_weak_deps=False \
+             --assumeyes --destdir="${DNF_CACHE}" ${xpkg}
+      done
+   fi
+   rm -f in_output
+
+   # attempt to keep metadata especially the checksum up to date
+   dnf makecache
+
+   PKG_CNT="$(find "${DNF_CACHE}" -name \*.rpm | wc -l)"
+   echo "${RHEL_PKG_SECT} Package count ${PKG_CNT}"
 fi
 
 # index the cache
 
-mkdir -p "$ROOTFS/etc/dnf"
-#echo "$CACHE/.." > "$ROOTFS/etc/rhel/repositories"
+#** for now don't try to copy/set up dnf/yum for EVE runtime
+#**  ROOTFS is not defined as well
+#** mkdir -p "$ROOTFS/etc/dnf"
+#** echo "$CACHE/.." > "$ROOTFS/etc/rhel/repositories"
--- pkg/rhel/eve-rhel-deploy.sh
+++ pkg/rhel/eve-rhel-deploy.sh
@@ -19,21 +19,7 @@
 #   eve-rhel-deploy.sh 9.2
 set -e
 
-SUSE_VERSION=${1:-9,2}
-
-#* Zypper return values are more complex, and some
-#*  types of errors we can ignore
-#*  see https://en.opensuse.org/SDB:Zypper_manual_(plain)
-#*  the "EXIT CODES" section
-#** function check_zypp_error () {
-#**     local rval=$1
-#**     if [ $rval -ge 100 ] && [ $rval -ne 105 ] && [ $rval -ne 101 ]
-#**     then
-#**         echo "Zypper rval=${rval}, swizzle to 0" >&2
-#**         rval=0
-#**     fi
-#**     return $rval
-#** }
+RHEL_VERSION=${1:-9.2}
 
 case "$(uname -m)" in
    x86_64) BUILD_PKGS="$BUILD_PKGS $BUILD_PKGS_amd64"
@@ -52,24 +38,69 @@ case "$(uname -m)" in
            ;;
 esac
 
-#** yum --terse --non-interactive modifyrepo --no-refresh --keep-packages --all
+#** keep track of dnf configuration for now
+#**  HACK remove or debug only at some point
+cat /etc/dnf/dnf.conf
 
+#** my debug list of cache contents, side effect of section
+#**  as part of the path name
+#find /var/cache/dnf -name \*.rpm
+#dnf -v makecache
+DOGO=
 if [ "$BUILD_PKGS" != " " ]
 then
-   yum --nodocs --ignore-unknown --assumeyes --cacheonly \
-         install $BUILD_PKGS
-   z_rel=$?
+   LCL_PKGS=
+   for xpkg in $BUILD_PKGS
+   do
+      #** should check for go${GOVER} only as allow, with error out
+      #**  if the go version requested is not the expected
+      #**  I can be as opinionated as linuxkit
+      if [ "${xpkg}" = "go" ] || [ "${xpkg}" = "go[0-9]\.[0-9][0-9]*\.[0-9]*" ]
+      then
+         DOGO="true"
+      else
+         LCL_PKGS="${LCL_PKGS} ${xpkg}"
+      fi
+   done
+   #** should work with --cacheonly (-C) set, need to fully populate
+   #**  the package cache and try again, just appears to ignore it
+   #**  will also try higher debug and error reporting levels
+   dnf -v -C --nodocs --assumeyes --setopt=install_weak_deps=False \
+         --allowerasing install ${LCL_PKGS}
 fi
-#** check_zypp_error $z_rel
 
-#** rm -rf /out
-#** mkdir /out
-#** tar -C "/mirror/$SUSE_VERSION/rootfs" -cf- . | tar -C /out -xf-
+gover="$(cat /eve/gover)"
+if [ "${DOGO}" = "true" ]; then
+   this_arch="$(cat /eve/this_arch)"
+   tar -C /usr/local -xzf /eve/go${gover}.linux-${this_arch}.tar.gz
+   ls -lr /usr/local/go /usr/local/go/bin
+   cd /usr/bin && ln -s /usr/local/go/bin/go .
+   cd /usr/bin && ln -s /usr/local/go/bin/gofmt .
+   cd /
+fi
+
+rm -rf /out
+mkdir /out
+tar -C "/mirror/$RHEL_VERSION/rootfs" -cf- . | tar -C /out -xf-
 
 if [ "$PKGS" != " " ]
 then
-    yum --nodocs --ignore-unknown --installroot /out --cacheonly \
-           install $PKGS
-    z_rel=$?
+   LCL_PKGS=
+   for xpkg in $PKGS
+   do
+      #** see comment above, craete a shell function for this??
+      if [ "${xpkg}" = "go" ] || [ "${xpkg}" = "go[0-9]\.[0-9][0-9]*\.[0-9]*" ]
+      then
+         targetarch="$(cat /eve/targetarch)"
+         tar -C /out/usr/local -xzf /eve/go${gover}.linux-${targetarch}.tar.gz
+         cd /out/usr/bin && ln -s /usr/local/go/bin/go .
+         cd /out/usr/bin && ln -s /usr/local/go/bin/gofmt .
+         cd /
+      else
+         LCL_PKGS="${LCL_PKGS} ${xpkg}"
+      fi
+   done
+   dnf -C --nodocs --installroot /out --cacheonly \
+       --setopt=install_weak_deps=False \
+           --allowerasing install $PKGS
 fi
-#** check_zypp_error $z_rel
