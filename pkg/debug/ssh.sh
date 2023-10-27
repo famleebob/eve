1a87120eb9b4bccb5766b1e20348dd98ad3d4e72
Gerald (Bob) Lee <bob@famleehouse.net>
2023-10-27 14:22:38 -0700

    live: add libusbx, and mount point dirs
    Needed to add libusbx to get the expected/required version of libusb.
    Also, added some mount pointer required on the default root file
    system.
    
    The debug container failed to run.  From logs determined that
    `/bin/eve` was not found.  Turns out that the copy in of the utility
    from dom0-ztools, to avoid problems with RHEL root directory symbolic
    links the `eve` script ended up in the `/usr/bin` directory.  Needed
    to point the debug container to the correct location.
    
    Proved and used a new way to copy in the muscl libc binaries so
    containerd could use them.
    
    Removed debugging from `pkg/debug/ssh.sh` (that is -x removed).
    
    Update xen-tools to get the version of libusb required to run
    correctly.  Otherwise, can't even try to run a container.
    
    `mkimage-raw-efi` needed more build packages, and now builds
    busybox (can be used for raw installer).
    
    Signed-off-by: Gerald (Bob) Lee <bob@famleehouse.net>

--- pkg/debug/ssh.sh
+++ pkg/debug/ssh.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+#* HACK, allows eve to run which uses containerd/ctr which
+#*  come from linuxkit, and for now require the libc musl
+if [ ! -e /lib/libc.musl-x86_64.so.1 ]
+then
+    cp /hostfs/lib/ld-musl-x86_64.so.1 \
+       /hostfs/lib/libc.musl-x86_64.so.1 \
+            /lib
+fi
+
 # setting things up for being able to access linux kernel symbols
 echo 0 >  /proc/sys/kernel/kptr_restrict
 echo -1 > /proc/sys/kernel/perf_event_paranoid
